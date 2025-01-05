f13c7485d61b3196be1dd273b6daa7bd31d37e8d
Heldson L Silva <heldson@kanttum.com>
2025-01-05 19:34:19 -0300

    ✨ Feat: Create Loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,12 @@
+import "react-activity/dist/library.css";
+import { Sentry } from "react-activity";
+import { LoadingContainer } from "./styles";
+
+export function Loading() {
+  return (
+    <LoadingContainer>
+      <Sentry />
+      <strong>CARREGANDO</strong>
+    </LoadingContainer>
+  );
+}
---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const LoadingContainer = styled.div`
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  position: absolute;
+  right: 50%;
+  top: 50%;
+  transform: translate(50%);
+`;
