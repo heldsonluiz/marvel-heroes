94ae43b28575f486122ae1fc5abba0ad0de4dbf7
Heldson L Silva <heldson@kanttum.com>
2025-01-05 19:37:20 -0300

    🚧 Feat: use API service to fetch Heroes list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,32 +1,48 @@
-import { HomeContainer } from "./styles";
+import { HeaderContainer, HomeContainer, HomeContent } from "./styles";
+
+import logo from "../../assets/logo.svg";
+
+import { useState, useEffect } from "react";
+import { api, authenticate } from "../../services/api";
 
 import { HeroProps } from "../../components/Hero";
 import { HeroList } from "../../components/HeroList";
-
-const mockProps: HeroProps[] = [
-  {
-    id: 1011334,
-    name: "3-D Man",
-    thumbnail: {
-      path: "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784",
-      extension: "jpg",
-    },
-  },
-  {
-    id: 1011332,
-    name: "3-D Man",
-    thumbnail: {
-      path: "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784",
-      extension: "jpg",
-    },
-  },
-];
+import { Loading } from "../../components/Loading";
 
 export function Home() {
+  const [heroes, serHeroes] = useState<HeroProps[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const requestCharacters = async () => {
+    try {
+      const { data } = await api.get(`/characters?${authenticate()}`);
+
+      serHeroes(data.data.results);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    requestCharacters();
+  }, []);
+
   return (
     <HomeContainer>
-      <h1>Home</h1>
-      <HeroList heroes={mockProps} />
+      <HeaderContainer>
+        <img src={logo} alt="" />
+        <h1>Explore o universo</h1>
+        <span>
+          Mergulhe no domínio deslumbrante de todos os personagens clássicos que
+          você ama - e aqueles que você descobrirá em breve!
+        </span>
+      </HeaderContainer>
+      {/* <div>Search</div> */}
+      <div>{/* <div>controls</div> */}</div>
+      <HomeContent>
+        {isLoading ? <Loading /> : <HeroList heroes={heroes} />}
+      </HomeContent>
     </HomeContainer>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -7,3 +7,30 @@ export const HomeContainer = styled.div`
   align-content: flex-start;
   justify-content: center;
 `;
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: fit-content;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  padding-bottom: 16px;
+
+  h1 {
+    color: ${(props) => props.theme["gray-500"]};
+    font-size: 2rem;
+    text-transform: uppercase;
+    letter-spacing: -1px;
+  }
+
+  span {
+    font-size: 0.875rem;
+    color: ${(props) => props.theme["gray-300"]};
+  }
+`;
+
+export const HomeContent = styled.div`
+  width: 100%;
+  flex-grow: 1;
+`;
