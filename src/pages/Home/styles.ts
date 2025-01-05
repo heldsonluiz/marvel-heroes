ecb7d4576461a4afc673b66f2b1ff3158b4f5b21
Heldson L Silva <heldson@kanttum.com>
2025-01-05 17:59:26 -0300

    ♻️ Change: refactor home page

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  justify-content: center;
+`;
