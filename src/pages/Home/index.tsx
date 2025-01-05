701c6a795b50f2759afdb6de746007818638c689
Heldson L Silva <heldson@kanttum.com>
2025-01-05 19:31:18 -0300

    🤡 ?: Use mocked data to view components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,32 @@
 import { HomeContainer } from "./styles";
 
+import { HeroProps } from "../../components/Hero";
+import { HeroList } from "../../components/HeroList";
+
+const mockProps: HeroProps[] = [
+  {
+    id: 1011334,
+    name: "3-D Man",
+    thumbnail: {
+      path: "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784",
+      extension: "jpg",
+    },
+  },
+  {
+    id: 1011332,
+    name: "3-D Man",
+    thumbnail: {
+      path: "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784",
+      extension: "jpg",
+    },
+  },
+];
+
 export function Home() {
   return (
     <HomeContainer>
       <h1>Home</h1>
+      <HeroList heroes={mockProps} />
     </HomeContainer>
   );
 }
