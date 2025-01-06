1b61e1aa564ac1b3375ea9df5d574eed91c52b0a
Heldson L Silva <heldson@kanttum.com>
2025-01-06 07:22:48 -0300

    ✨ Feat: create a FavoriteIcon component

---
+++ src/components/FavoriteIcon/index.tsx
@@ -0,0 +1,39 @@
+import {
+  FavoriteButtonContainer,
+  FavoriteButtonContainerOff,
+  FavoriteButtonContainerOn,
+  FavoriteContainer,
+  FavoriteLabel,
+} from "./styles";
+
+interface FavoriteIconProps {
+  active: boolean;
+  loading: boolean;
+  onClickAction: () => void;
+  label?: string;
+}
+
+export function FavoriteIcon({
+  active,
+  loading,
+  label,
+  onClickAction,
+}: FavoriteIconProps) {
+  const handleClickAction = () => {
+    if (loading) return;
+    onClickAction();
+  };
+
+  return (
+    <FavoriteContainer>
+      <FavoriteButtonContainer onClick={handleClickAction}>
+        {active ? (
+          <FavoriteButtonContainerOn />
+        ) : (
+          <FavoriteButtonContainerOff />
+        )}
+      </FavoriteButtonContainer>
+      <FavoriteLabel>{label}</FavoriteLabel>
+    </FavoriteContainer>
+  );
+}
