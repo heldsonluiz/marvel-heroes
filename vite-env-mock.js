e544e943f7867069e86a8c38ef354a2d05b47da9
Heldson L Silva <heldson@kanttum.com>
2025-01-08 10:23:31 -0300

    🚧 Feat: configure tests with Jest

---
+++ vite-env-mock.js
@@ -0,0 +1,7 @@
+global.importMeta = {
+  env: {
+    VITE_API_KEY_PUBLIC:'f1fc6b41a1290ebfe4d8737bf4c173c8',
+    VITE_API_KEY_PRIVATE:'37c1f14a96b053fa565ee58d88555c29ef0c4b2d',
+    VITE_API_BASE_URL:'https://gateway.marvel.com/v1/public/'
+  },
+};
